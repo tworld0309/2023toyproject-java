3bf716e7ad9d332c87c3fb9ec13bf35246342403
thj0309 <thj0309@hotmail.com>
2023-06-17 16:48:49 +0900

    batch

---
+++ server-batch/src/main/java/com/hotel/serverbatch/ServerBatchApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.serverbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerBatchApplication.class, args);
+    }
+
+}
---
+++ server-batch/src/test/java/com/hotel/serverbatch/ServerBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.serverbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
